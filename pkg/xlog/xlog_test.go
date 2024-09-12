27c0bc5ad853af16fee7ac386bdf8dc9c921bed9
LiusCraft <meliuscraft@gmail.com>
2024-09-12 10:07:47 +0800

    chore: add xlog test concurrency ut

--- pkg/xlog/xlog_test.go
+++ pkg/xlog/xlog_test.go
@@ -1,6 +1,9 @@
 package xlog
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestPrintLog(t *testing.T) {
 	xl := NewLogger()
@@ -8,4 +11,20 @@ func TestPrintLog(t *testing.T) {
 	xl.Infof("test %s", "info")
 	xl.Warnf("test %s", "warn")
 	xl.Errorf("test %s", "error")
+}
+
+func TestConcurrency(t *testing.T) {
+	xl := NewLogger()
+	g := &sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		g.Add(1)
+		go func() {
+			defer func() { g.Done() }()
+			xl.Debugf("test %s, %d", "debug", i)
+			xl.Infof("test %s, %d", "info", i)
+			xl.Warnf("test %s, %d", "warn", i)
+			xl.Errorf("test %s, %d", "error", i)
+		}()
+	}
+	g.Wait()
 }
